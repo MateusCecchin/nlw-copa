d45e8df6d1437d54c8c5c4d746c8d5b76236851b
MateusCecchin <mateusfernandescn@gmail.com>
2022-11-05 13:04:07 -0400

    initial commit

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import {NativeBaseProvider, Center, Text} from "native-base"
+import { Loading } from "../components/loading"
+
+export function SignIn() {
+    return (
+        <Center flex={1} bgColor="gray.900">
+                <Text color="white" fontSize={24}>SignIn</Text>
+      </Center>
+    )
+}
